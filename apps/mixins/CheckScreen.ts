2507704260d8062c73cf9b0d4517da910003c1bf
kojiyamauchi <all.the.small.things@me.com>
2019-04-12 20:07:00 +0900

    update CheckScreen.ts

--- apps/mixins/CheckScreen.ts
+++ apps/mixins/CheckScreen.ts
@@ -17,15 +17,13 @@ export class CheckScreen extends Vue {
   checkDirection() {
     if (this.checkMobile.test(navigator.userAgent)) {
       if (this.$store.state.isResize) return this.$store.commit('offResize')
-      return screen.width > screen.height
-        ? this.$store.commit('onLandscape')
-        : this.$store.commit('offLandscape')
+      return screen.width > screen.height ? this.$store.commit('onLandscape') : this.$store.commit('offLandscape')
     }
   }
 
   // PC Only.
   checkResize() {
-    if (!navigator.userAgent.match(this.checkMobile)) {
+    if (!this.checkMobile.test(navigator.userAgent)) {
       if (this.$store.state.isLandscape) return this.$store.commit('offLandscape')
       if (!this.$store.state.isResize) return this.$store.commit('onResize')
       clearTimeout(this.timeID!)
@@ -36,12 +34,16 @@ export class CheckScreen extends Vue {
   }
 
   created() {
-    this.checkDirection()
-    window.addEventListener('orientationchange', this.checkDirection)
-    window.addEventListener('resize', this.checkResize)
+    if (process.browser) {
+      this.checkDirection()
+      window.addEventListener('orientationchange', this.checkDirection)
+      window.addEventListener('resize', this.checkResize)
+    }
   }
 
   beforeDestroy() {
-    window.removeEventListener('resize', this.checkResize)
+    if (process.browser) {
+      window.removeEventListener('resize', this.checkResize)
+    }
   }
 }
