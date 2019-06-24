7b46444d16af71f4cb63af6e7b93ac2fd7eafe2a
kojiyamauchi <all.the.small.things@me.com>
2019-06-24 23:58:19 +0900

    move CheckScreen.ts

--- apps/plugins/CheckScreen.ts
+++ apps/plugins/CheckScreen.ts
@@ -4,36 +4,36 @@ import { Mixin } from 'vue-mixin-decorator'
 @Mixin
 export class CheckScreen extends Vue {
   // Types.
-  checkMobile: RegExp
-  timeID: NodeJS.Timer | null
+  private checkMobile: RegExp
+  private timeID: NodeJS.Timer | null
 
-  constructor() {
+  public constructor() {
     super()
     this.checkMobile = /(iPhone|iPad|iPod|Android)/i
     this.timeID = null
   }
 
   // SP Only.
-  checkDirection() {
+  private checkDirection(): void {
     if (this.checkMobile.test(navigator.userAgent)) {
-      if (this.$store.state.isResize) return this.$store.commit('offResize')
+      if (this.$store.state.isResize) this.$store.commit('offResize')
       return screen.width > screen.height ? this.$store.commit('onLandscape') : this.$store.commit('offLandscape')
     }
   }
 
   // PC Only.
-  checkResize() {
+  private checkResize(): void {
     if (!this.checkMobile.test(navigator.userAgent)) {
-      if (this.$store.state.isLandscape) return this.$store.commit('offLandscape')
-      if (!this.$store.state.isResize) return this.$store.commit('onResize')
+      if (this.$store.state.isLandscape) this.$store.commit('offLandscape')
+      if (!this.$store.state.isResize) this.$store.commit('onResize')
       clearTimeout(this.timeID!)
-      this.timeID = setTimeout(() => {
+      this.timeID = setTimeout((): void => {
         this.$store.commit('offResize')
       }, 750)
     }
   }
 
-  created() {
+  private created(): void {
     if (process.browser) {
       this.checkDirection()
       window.addEventListener('orientationchange', this.checkDirection)
@@ -41,7 +41,7 @@ export class CheckScreen extends Vue {
     }
   }
 
-  beforeDestroy() {
+  private beforeDestroy(): void {
     if (process.browser) {
       window.removeEventListener('resize', this.checkResize)
     }
